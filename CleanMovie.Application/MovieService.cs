9c7eb05090178419954ad51295af37dc1cc34da2
javadansari65 <rayano_co@yahoo.com>
2024-02-23 00:15:06 +0330

    firstcommit

---
+++ CleanMovie.Application/MovieService.cs
@@ -0,0 +1,32 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        //Constructor Dependency Injection
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public Movie CreateMovie(Movie movie)
+        {
+            var addMovie = _movieRepository.CreateMovie(movie);
+            return addMovie;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            var movies = _movieRepository.GetAllMovies();
+            return movies;
+        }
+    }
+}
---
+++ CleanMovie.Domain/Rental.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanMovie.Domain
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime RentalExpiry { get; set; }
+        public decimal TotalCost { get; set; }
+
+        //One to many relationship
+        public ICollection<Member> Members { get; set; }
+
+        //Many to Many Relation
+        public IList<MovieRental> MovieRentals { get; set; }
+    }
+}
